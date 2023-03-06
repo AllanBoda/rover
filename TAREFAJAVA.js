1d914492ec0897966787ec4e7e4ae733763b30af
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2023-03-06 14:47:30 -0300

    Add files via upload

---
+++ TAREFAJAVA.js
@@ -0,0 +1,72 @@
+let Result="";
+var table=1;
+
+function apertaDireita() {
+    if(Result == ""){
+    Result += "Direita";
+    
+    }else{
+        Result += ", Direita";
+    }
+
+    document.getElementById("lado").innerHTML = "Direção= " + Result;
+
+    if(table != 4 && table != 8 && table != 12 && table != 16){
+        table+=1
+    }
+    
+}
+
+function apertaEsquerda() {
+    if(Result == ""){
+    Result += "Esquerda";
+        
+    }else{
+    Result += ", Esquerda";
+    }
+
+    document.getElementById("lado").innerHTML = "Direção= " + Result;
+
+    if(table != 1 && table != 5 && table != 9 && table != 13){
+        table+=1
+    }
+   
+}
+
+function apertaCima() {
+    if(Result == ""){
+    Result += "Cima";
+        
+    }else{
+     Result += ", Cima";
+    }
+
+    document.getElementById("lado").innerHTML = "Direção= " + Result;
+
+    if(table != 1 && table != 2 && table != 3 &&table != 4){
+        table+=1
+    }
+
+}
+
+function apertaBaixo() {
+    if(Result == ""){
+    Result += "Baixo";
+        
+    }else{
+    Result += ", Baixo";
+    }
+
+    document.getElementById("lado").innerHTML = "Direção= " + Result;
+
+    if(table != 13 && table != 14 && table != 15 && table != 16){
+        table+=1
+    }
+    
+}
+
+function colocaResult() {
+    document.getElementById("img").src = "C:\Users\lucas.gomes\Desktop\Proj\Table-"+table+".jpg";
+
+    document.getElementById("resultado").innerHTML = "Lugar="+table;
+}
